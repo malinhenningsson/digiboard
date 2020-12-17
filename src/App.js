8b6296343dbcc525e606a3e641bd9cc1272c8ca6
Malin Henningsson <malinhenningsson@live.se>
2020-12-17 17:32:19 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import { Container } from 'react-bootstrap';
 import Room from './screens/Room';
 import Lobby from './screens/Lobby';
+import NotFound from './screens/NotFound';
 
 
 function App() {
   return (
-    <Container fluid>
-      <Lobby />
-      <Room />
-    </Container>
+	  <Router>
+		<Container fluid>
+			<Routes>
+				<Route path="/">
+					<Lobby />
+				</Route>
+				<Route path="/channel/:channelId">
+					<Room />
+				</Route>
+				<Route path="*" element={ <NotFound /> } />
+			</Routes>
+		</Container>
+	  </Router>
   );
 }
 
