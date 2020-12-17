f2b0fb0a14bae61f213c893bd7fcad637c05e228
Malin Henningsson <malinhenningsson@live.se>
2020-12-17 18:34:02 +0100

    Simple chat styling

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Chat = () => {
     return (
-        <div>
-            This is where the chat will go
+        <div id="chat-wrapper">
+            <div className="online-users">
+                <h1 className="bold">Online:</h1>
+                <ul>
+                    <li>User 1</li>
+                    <li>User 2</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li className="your-msg"><span className="bold">User 1:</span> Hello!</li>
+                    <li className="others-msg"><span className="bold">User 2:</span> Hey!</li>
+                    <li className="others-msg"><span className="bold">User 2:</span> Whats up?</li>
+                </ul>
+                <input type="text" placeholder="Chat with the other users" />
+            </div>
         </div>
     )
 }
