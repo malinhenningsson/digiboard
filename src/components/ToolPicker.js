107aab533fea44dda18593cd25e8291a015e33d4
Malin Henningsson <malinhenningsson@live.se>
2020-12-18 10:51:39 +0100

    Add placeholder content toolpicker and styling

--- src/components/ToolPicker.js
+++ src/components/ToolPicker.js
@@ -3,7 +3,17 @@ import React from 'react'
 const ToolPicker = () => {
     return (
         <div id="toolpicker-wrapper">
-            This is where the toolpicker will go
+            <ul>
+                <li>Black</li>
+                <li>Red</li>
+                <li>Blue</li>
+                <li>Green</li>
+                <li>Yellow</li>
+                <li>Orange</li>
+            </ul>
+            <div>
+                <button id="download">Download</button>
+            </div>
         </div>
     )
 }
