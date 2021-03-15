3720a49be8219024829e7677d7d53a9e3e552c85
Malin Henningsson <malin.henningsson@jayway.com>
2021-03-15 14:03:09 +0100

    change to getting random room name

--- src/screens/Lobby.js
+++ src/screens/Lobby.js
@@ -1,30 +1,37 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { usePubnub } from "../contexts/PubNubContext";
 
 const Lobby = () => {
-    const [username, setUsername] = useState(null);
-    const [roomname, setRoomname] = useState(null);
-    const navigate = useNavigate();
-    const { subscribeToChannel, updateUserInfo } = usePubnub();
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        updateUserInfo(username, roomname);
-        subscribeToChannel(roomname, username);
-        navigate(`/channel/${roomname}`, {state: {username}});
-    }
+  const [username, setUsername] = useState(null);
+  const navigate = useNavigate();
+  const { subscribeToChannel, updateUserInfo } = usePubnub();
 
-    return (
-        <div id="lobby-wrapper">
-            <h1>DigiBoard</h1>
-            <form id="lobby-form" onSubmit={handleSubmit}>
-                <input type="text" placeholder="Choose a username" onChange={(e) => setUsername(e.target.value)} />
-                <input type="text" placeholder="Choose a roomname" onChange={(e) => setRoomname(e.target.value)} />
-                <button>Create room</button>
-            </form>
-        </div>
-    )
-}
+  const getRandomRoomName = () => {
+    return Math.random().toString(20).substr(2, 10);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const roomname = getRandomRoomName();
+    updateUserInfo(username, roomname);
+    subscribeToChannel(roomname, username);
+    navigate(`/channel/${roomname}`, { state: { username } });
+  };
+
+  return (
+    <div id="lobby-wrapper">
+      <h1>DigiBoard</h1>
+      <form id="lobby-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Choose a username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button>Create room</button>
+      </form>
+    </div>
+  );
+};
 
 export default Lobby;
