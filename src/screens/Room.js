b9039aa548612937af6fb0f49f02a5cd60f7951c
Malin Henningsson <malin.henningsson@jayway.com>
2021-04-27 15:29:14 +0200

    add leave room btn instead of useeffect

--- src/screens/Room.js
+++ src/screens/Room.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams, useNavigate } from "react-router-dom";
 import Whiteboard from "../components/Whiteboard";
 import Chat from "../components/Chat";
 import InviteUsers from "../components/InviteUsers";
 import { usePubnub } from "../contexts/PubNubContext";
 import PopUp from "../components/PopUp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft
+} from "@fortawesome/free-solid-svg-icons";
 
 const Room = () => {
   const [showInviteUsers, setShowInviteUsers] = useState(false);
   const [showInfoMessage, setShowInfoMessage] = useState(false);
   const location = useLocation();
+  const navigate = useNavigate();
   const { channelId } = useParams();
   const { unsubscribeFromChannel, infoMessage } = usePubnub();
 
@@ -25,12 +30,13 @@ const Room = () => {
     }
   }, [infoMessage]);
 
-  // Unsubscribe from channel when leaving the room
-  useEffect(() => {
-    return () => {
+  const handleLeaveEvent = () => {
+    const res = window.confirm("Are you sure you want to leave the room?");
+    if (res) {
       unsubscribeFromChannel(channelId);
+      navigate("/");
     }
-  }, []);
+  };
 
   return (
     <>
@@ -55,6 +61,14 @@ const Room = () => {
           }
           channelName={channelId}
         />
+      </div>
+      <div>
+        <FontAwesomeIcon
+            title="leave channel"
+            className="fa-icon-leave"
+            icon={faChevronLeft}
+            onClick={() => handleLeaveEvent()}
+          />
       </div>
       {showInviteUsers && (
         <InviteUsers
