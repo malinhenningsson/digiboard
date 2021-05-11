7d67c7a00264cdf3726b92541e27b5b9848d031c
Malin Henningsson <malinhenningsson@live.se>
2021-05-11 10:57:07 +0200

    styling not found page

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
 	return (
-		<div>
-			404 Not Found
+		<div id="not-found">
+			<div>
+				<h1>404 Not Found</h1>
+				<h2>Return to <Link to="/">home page</Link></h2>
+			</div>
 		</div>
 	)
 }
